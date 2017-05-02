d075e33ff1ed8847a519e11bb8f5fb81f1832f1d
fcu-d0441320 <d0441320@fcu.edu.tw>
2017-05-02 20:52:48 +0800

    finish add note

--- app/src/main/java/tw/hysh/nicky/android_hw6/MainActivity.java
+++ app/src/main/java/tw/hysh/nicky/android_hw6/MainActivity.java
@@ -1,22 +1,48 @@
 package tw.hysh.nicky.android_hw6;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import  android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    public Button add;
+    static  String DATABASE_TABLE="Notebook";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
+    private View.OnClickListener toAdd = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent nextPage = new Intent(MainActivity.this,EditNote.class);
+            nextPage.putExtra("NoteTitle","title");
+            startActivity(nextPage);
+        }
+    };
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        add = (Button) findViewById(R.id.btn_addNewNote);
+        add.setOnClickListener(toAdd);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
     }
 
     @Override
@@ -48,5 +76,47 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+
+    }
+
+
+    class DBOpenHelper extends SQLiteOpenHelper {
+        public DBOpenHelper(Context context) {
+            super(context, "demo.db", null, 2);
+        }
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("create table " +
+                    DATABASE_TABLE + "( title, body);");
+        }
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldV, int newV) {
+            Log.d("LINCYU", "onUpgrade: " + oldV + " -> " + newV);
+            db.execSQL("alter table " + DATABASE_TABLE +
+                    " add column date;");
+        }
     }
 }
